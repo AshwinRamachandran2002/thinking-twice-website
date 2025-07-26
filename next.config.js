02f097a8404c3783ba53923b2c8109e8cdd8714c
Harshvardhan Agarwal <74874642+draco976@users.noreply.github.com>
2025-07-26 16:28:38 -0700

    new website

--- next.config.js
+++ next.config.js
@@ -2,13 +2,11 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
-  basePath: '',
   images: {
     unoptimized: true
   },
   typescript: {
-    ignoreBuildErrors: false,
+    ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
